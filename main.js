ab77a463c763fdd2456b1ebc0cb5954d2dc477e3
overlord70 <s37776270@gmail.com>
2024-03-30 13:57:51 +0500

    20%

---
+++ main.js
@@ -0,0 +1,83 @@
+import { getData } from "./modules/http";
+import { CreateHeader } from "/modules/ui";
+CreateHeader()
+const options = document.querySelectorAll('.options p')
+let prev = 0
+options.forEach((opt, idx) => {
+    opt.onclick = () => {
+        options[prev].id = ''
+        opt.id = 'active_option'
+        prev = idx
+    }
+})
+const movies = document.querySelector('.movies')
+const photos_of_movies = document.querySelector('.photos_of_movies')
+  function reload(arr, place) {
+    place.innerHTML = ''
+    for (const item of arr) {
+        const movie = document.createElement('div')
+        const relative = document.createElement('div')
+        const img_bg = document.createElement('div')
+        const score_of_movie = document.createElement('div')
+        const num = document.createElement('p')
+        const title = document.createElement('h2')
+        const type = document.createElement('p')
+        movie.className= 'movie'
+        relative.classList.add('relative')
+        img_bg.className = 'img_bg'
+        img_bg.style.background = `url(https://image.tmdb.org/t/p/original${item.poster_path})`
+        img_bg.style.backgroundSize = 'contain'
+        img_bg.style.backgroundRepeat = 'no-repeat'
+        score_of_movie.className = 'score_of_movie'
+        num.id = 'num'
+        num.innerHTML = item.vote_average
+        title.className = 'title'
+        title.innerHTML = item.title
+        type.className = 'type'
+        type.innerHTML = item.original_language
+        score_of_movie.append(num)
+        relative.append(img_bg, score_of_movie)
+        movie.append(relative, title, type)
+        place.append(movie)
+    }
+  }
+  const btn = document.querySelector('.all_new_ones')
+const get = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODAzYWUxZGEwOGU3M2RmM2ZjMTI2OGMzNTE2NWNjMiIsInN1YiI6IjY0MjdlZWY4OGE4OGIyMDBkNTMyOGQ1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8WByVMOVhx2M7eo1SLPb3cPkt2NSfzLg53Afm1_dr-M'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', get)
+    .then(response => response.json())
+    .then(response => {
+        reload(response.results.slice(0, 8), photos_of_movies)
+        btn.onclick = () => {
+            if(btn.id === 'all'){
+                btn.id = ''
+                btn.innerHTML = 'Все новинки'
+                reload(response.results.slice(0, 8), photos_of_movies)
+            } else {
+                btn.id = 'all'
+                btn.innerHTML = 'вернуться'
+                reload(response.results, photos_of_movies)
+            }
+        }
+        console.log(response);
+    })
+    const get_2 = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODAzYWUxZGEwOGU3M2RmM2ZjMTI2OGMzNTE2NWNjMiIsInN1YiI6IjY0MjdlZWY4OGE4OGIyMDBkNTMyOGQ1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8WByVMOVhx2M7eo1SLPb3cPkt2NSfzLg53Afm1_dr-M'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', get_2)
+        .then(response => response.json())
+        .then(response =>{
+            
+            console.log(response)
+        })
--- modules/ui.js
+++ modules/ui.js
@@ -1,3 +1,85 @@
 export function CreateHeader() {
-    
+//     <header id="header">
+//     <div class="left">
+//       <div class="logo">
+//         <img src="/public/img/XMLID 1219.png" alt="">
+//         <h1>
+//           Kino<span id="area">area</span>
+//         </h1>
+//       </div>
+//       <img src="/public/img/   .png" alt="">
+//     </div>
+//     <nav class="main">
+//       <a href="#">Афиша</a>
+//       <a href="#">Медиа</a>
+//       <a href="#">Фильмы</a>
+//       <a href="#">Актёры</a>
+//       <a href="#">Новости</a>
+//       <a href="#">Подборки</a>
+//       <a href="#">Категории</a>
+//     </nav>
+//     <div class="right">
+//       <button id="first_one">
+//         <img src="/public/img/Vector.png" alt="">
+//       </button>
+//       <button id="second_one">
+//         Войти
+//       </button>
+//     </div>
+//    </header>
+    const header = document.createElement('header')
+    const left = document.createElement('div')
+    const logo = document.createElement('div')
+    const img_1 = document.createElement('img')
+    const h1 = document.createElement('h1')
+    const area = document.createElement('span')
+    const img_2 = document.createElement('img')
+    const main = document.createElement('nav')
+    const a_1 = document.createElement('a')
+    const a_2 = document.createElement('a')
+    const a_3 = document.createElement('a')
+    const a_4 = document.createElement('a')
+    const a_5 = document.createElement('a')
+    const a_6 = document.createElement('a')
+    const a_7 = document.createElement('a')
+    const right = document.createElement('div')
+    const first_one = document.createElement('button')
+    const img_3 = document.createElement('img')
+    const second_one = document.createElement('button')
+    header.id = 'header'
+    left.className = 'left'
+    right.className = 'right'
+    main.className = 'main'
+    area.id = 'area'
+    logo.className = 'logo'
+    area.innerHTML = 'area'
+    h1.innerHTML = 'Kino'
+    img_1.src = '/public/img/XMLID 1219.png'
+    img_2.src = '/public/img/   .png'
+    a_1.innerHTML = 'Афиша'
+    a_2.innerHTML = 'Медиа'
+    a_3.innerHTML = 'Фильмы'
+    a_4.innerHTML = 'Актёры'
+    a_5.innerHTML = 'Новости'
+    a_6.innerHTML = 'Подборки'
+    a_7.innerHTML = 'Категории'
+    a_1.href = '#'
+    a_2.href = '#'
+    a_3.href = '#'
+    a_4.href = '#'
+    a_5.href = '#'
+    a_6.href = '#'
+    a_7.href = '#'
+    first_one.id = 'first_one'
+    second_one.id = 'second_one'
+    second_one.innerHTML = 'Войти'
+    img_3.src = '/public/img/Vector.png'
+    first_one.append(img_3)
+    right.append(first_one, second_one)
+    h1.append(area)
+    main.append(a_1, a_2, a_3, a_4, a_5, a_6, a_7)
+    logo.append(img_1, h1)
+    left.append(logo, img_2)
+    header.append(left, main, right)
+    document.body.prepend(header)
 }
